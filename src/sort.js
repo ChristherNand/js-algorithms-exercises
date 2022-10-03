9f1651dc1ee35e156701d2575d2ec8c687b8695f
ChristherNand <chris.ha1801@gmail.com>
2022-10-03 10:37:59 -0500

    implementing sorting algorithm

--- src/sort.js
+++ src/sort.js
@@ -1,8 +1,13 @@
-//given an array of numbers sort it as shown here 
-//input [3,4,1,23,4,0,-1,7] output [-1,0,1,3,4,4,7,23]
+function sort(arr, direction) {
+  const newArray = [];
+  let numbers = arr.filter((n) => typeof n === "number");
 
-function sortAsc(arr){
-   
+  while (numbers.length > 0) {
+    const valToSort =
+      direction === "desc" ? Math.max(...numbers) : Math.min(...numbers);
+    newArray.push(valToSort);
+    numbers = numbers.filter((n) => n !== valToSort);
+  }
+  return newArray;
 }
-
-console.log(sortAsc([3,4,1,23,4,0,-1,7]))
+//console.log(sort([1,20,10,-1,0,5,4,null,"hello"],"asc")) // => [-1, 0, 1, 4, 5, 10, 20]
